29fd10dec76963c8c22ebe54daba9dbf70e9e987
Cecil <zfuaj@connect.ust.hk>
2020-06-03 21:36:06 +0800

    add camera switch

---
+++ CameraSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject front;
+    public GameObject back;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            front.SetActive(true);
+            back.SetActive(false);
+        }
+        else if(Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            front.SetActive(false);
+            back.SetActive(true);
+        }
+	}
+}
