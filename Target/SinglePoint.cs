46e1e8186da5146bbe2f6cd39dcc84f564a5db94
Cecil <zfuaj@connect.ust.hk>
2020-06-09 22:20:21 +0800

    point reaching

---
+++ Target/SinglePoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinglePoint : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject AttackPrefab;
+    void Start () {
+        Destroy(this.gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+       
+        
+        if (other.name != this.name)
+        {
+            Score score = other.gameObject.GetComponent<Score>();
+            score.Increase(1);
+            Debug.Log("add");
+
+            gameObject.SetActive(false);
+            GameObject attack = Instantiate(AttackPrefab);
+            ParticleSystem p = attack.GetComponent<ParticleSystem>();
+            p.Pause();
+            float time = p.main.duration;
+
+            attack.transform.position = gameObject.transform.position;
+            p.Play();
+
+            Debug.Log(other.name);
+            Destroy(attack, time);
+        }
+           
+
+    }
+}
---
+++ Target/SpawnBoom.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBoom : MonoBehaviour {
+
+    // Use this for initialization
+    public float radius;
+    public GameObject PointPrefab;
+
+    private float phi;
+    private float theta;
+    private float rho;
+
+    void Start () {
+        InvokeRepeating("createPoint", 0, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void createPoint()
+    {
+        rho = Random.Range(radius * 0.8f, radius);
+        theta = Random.Range(2.0f * Mathf.PI / 3.0f, 3.0f * Mathf.PI / 2.0f);
+        if (Mathf.Abs(theta - Mathf.PI) < Mathf.PI / 4.0f)
+        {
+            phi = Random.Range(0, Mathf.PI / 2.0f);
+        }
+        else
+        {
+            phi = Random.Range(0, Mathf.PI / 6.0f);
+        }
+
+        //rho = radius;
+        //theta = Mathf.PI / 2.0f;
+        //phi = Mathf.PI / 2.0f;
+
+        Vector3 center = new Vector3(rho * Mathf.Sin(phi) * Mathf.Cos(theta),
+                                     rho * Mathf.Sin(phi) * Mathf.Sin(theta),
+                                     rho * Mathf.Cos(phi));
+        GameObject point = Instantiate(PointPrefab, transform);
+        point.transform.localPosition = center;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(255, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.localPosition, radius);
+    }
+}
